e2f2991f52590468ec4c976c0696b3f60b4684f6
mrtuckie <arthur.lorencini@gmail.com>
2018-08-06 16:29:00 -0300

    06-08-2018 16:29:00

--- bash-script/git_push_padrao.sh
+++ bash-script/git_push_padrao.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+
+
 cd /home/pi/Desktop/Kurupira
 git status
 sleep 2
 git add .
 sleep 2
-git commit -m "atualização padrão"
+git commit -m "$(date +"%d-%m-%Y") $(date +"%T")"
 sleep 2
 git status
 sleep 2
---
+++ bash-script/time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+sleep 10
+NOW=$(date +"%m-%d-%Y")
+echo $NOW
+sleep 10
